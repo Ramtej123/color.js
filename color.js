7646ad5296af47ba45a1c268fb9e715830d572f9
Ramtej123 <122261377+Ramtej123@users.noreply.github.com>
2023-01-09 23:32:51 +0530

    updated

--- color.js
+++ color.js
@@ -1,17 +1,28 @@
-/* global coreui.Utils.rgbToHex */
-
-/**
- * --------------------------------------------------------------------------
- * CoreUI Boostrap Admin Template (v4.0.0-alpha.2): colors.js
- * Licensed under MIT (https://coreui.io/license)
- * --------------------------------------------------------------------------
- */
-document.querySelectorAll('.theme-color').forEach(function (element) {
-    var color = getComputedStyle(element, null).getPropertyValue('background-color');
-    var table = document.createElement('table');
-    table.classList.add('w-100');
-    table.innerHTML = "\n      <table class=\"w-100\">\n        <tr>\n          <td class=\"text-muted\">HEX:</td>\n          <td class=\"font-weight-bold\">" + coreui.Utils.rgbToHex(color) + "</td>\n        </tr>\n        <tr>\n          <td class=\"text-muted\">RGB:</td>\n          <td class=\"font-weight-bold\">" + color + "</td>\n        </tr>\n      </table>\n    ";
-    element.parentNode.appendChild(table);
-  });
-  //# sourceMappingURL=colors.js.map
+javascript
+let red = document.getElementById('red');
+let green = document.getElementById('green');
+let blue = document.getElementById('blue');
+let box = document.querySelector('div.neumorphism-3');
   
+let r = 0, g = 0, b = 0;
+  
+red.addEventListener("keyup", function (event) {
+    r = red.value;
+    if (!r)
+        r = 0;
+    box.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+});
+  
+green.addEventListener("keyup", function (event) {
+    g = green.value;
+    if (!g)
+    g = 0;
+    box.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+});
+  
+blue.addEventListener("keyup", function (event) {
+    b = blue.value;
+    if (!b)
+        b = 0;
+    box.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+});
